5b7f64d8b759582a7233b9c7f771a7bd74d5a1a9
Marian del Barrio-Torregrosa <marian.delbarrio@dipc.org>
2022-11-24 17:27:07 +0100

    set cylinder radius as parameter and CylinderPointSampler for the vertex

--- source/geometries/Fiber_cylinder.h
+++ source/geometries/Fiber_cylinder.h
@@ -8,11 +8,11 @@
 class G4Material;
 class G4GenericMessenger;
 class G4MaterialPropertiesTable;
-// class CylinderPointSampler;
+class CylinderPointSampler;
 
 namespace nexus
 {
-    // class CylinderPointSampler;
+    class CylinderPointSampler;
     class Fiber_cylinder: public GeometryBase
     {
         public:
@@ -24,8 +24,8 @@ namespace nexus
         private:
         G4double radius_;     //radius of the cylindrical optical fibre
         G4double length_;     //length of the cylindrical optical fibre
-        G4int n_fibers_;      //number of optical fibres
-        // CylinderPointSampler* cyl_vertex_gen_; // this creates photons homogeneously in a cylinder, so it is used for the tests but for the simulation we want to create the phton outside the fiber
+        G4double radius_cyl_; //radius of the cylinder
+        CylinderPointSampler* cyl_vertex_gen_; // this creates photons homogeneously in a cylinder, so it is used for the tests but for the simulation we want to create the phton outside the fiber
         G4GenericMessenger*   msg_;
     };
 } // namespace nexus
