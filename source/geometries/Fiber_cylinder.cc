7698bcc716d196453b62aadcd1a350a23458c070
Marian del Barrio-Torregrosa <marian.delbarrio@dipc.org>
2022-11-24 17:27:15 +0100

    set cylinder radius as parameter and CylinderPointSampler for the vertex

--- source/geometries/Fiber_cylinder.cc
+++ source/geometries/Fiber_cylinder.cc
@@ -1,11 +1,9 @@
-// we'll try to work with more than one fiber
-
 #include "Fiber_cylinder.h"
 #include "GenericWLSFiber.h"
 #include "MaterialsList.h"
 #include "OpticalMaterialProperties.h"
 #include "Visibilities.h"
-// #include "CylinderPointSampler.h"
+#include "CylinderPointSampler.h"
 #include "FactoryBase.h"
 
 #include <G4Tubs.hh>
@@ -30,8 +28,7 @@ using namespace CLHEP;
 
 REGISTER_CLASS(Fiber_cylinder,GeometryBase)
 
-// Fiber_cylinder::Fiber_cylinder():GeometryBase(), radius_(1.*mm), length_(1.*cm), cyl_vertex_gen_(0) {
-Fiber_cylinder::Fiber_cylinder():GeometryBase(), radius_(1.*mm), length_(1.*cm),  n_fibers_(10){
+Fiber_cylinder::Fiber_cylinder():GeometryBase(), radius_(1.*mm), length_(1.*cm),  radius_cyl_(1. *cm), cyl_vertex_gen_(0){
     msg_=new G4GenericMessenger(this,"/Geometry/Fiber_cylinder/","Control commands of geometry OpticalFibre");
 
     G4GenericMessenger::Command& radius_cmd =
@@ -46,12 +43,16 @@ Fiber_cylinder::Fiber_cylinder():GeometryBase(), radius_(1.*mm), length_(1.*cm),
     length_cmd.SetParameterName("length",false);
     length_cmd.SetRange("length>0.");
 
-    msg_->DeclareProperty("n_fibers",n_fibers_,"Number of optical fibres");
+    G4GenericMessenger::Command& radius_cyl_cmd =
+            msg_->DeclareProperty("radius_cyl",radius_cyl_,"Radius of the cylinder");
+    radius_cyl_cmd.SetUnitCategory("Length");
+    radius_cyl_cmd.SetParameterName("radius_cyl",false);
+    radius_cyl_cmd.SetRange("radius_cyl>0.");
 
-    // cyl_vertex_gen_ = new CylinderPointSampler(0.5*radius_, 0.5*length_, 0.,  0., G4ThreeVector(0., 0., 0.), 0);
+    cyl_vertex_gen_ = new CylinderPointSampler(0.5*radius_cyl_, 0.5*length_, 0.,  0., G4ThreeVector(0., 0., 0.), 0);
 }
 Fiber_cylinder::~Fiber_cylinder() {
-    // delete cyl_vertex_gen_;
+    delete cyl_vertex_gen_;
     delete msg_;
 }
 void Fiber_cylinder::Construct(){
@@ -70,30 +71,26 @@ void Fiber_cylinder::Construct(){
     G4Material* tpb = materials::TPB();
 
     // fibers loop
-    // int N = 70;
     GenericWLSFiber* fiber_;
     G4LogicalVolume* fiber_logic;
-    // G4double dif_phi = .6*2*pi/n_fibers_; // angular separation between fibers
-    G4double dif_phi = .5*2*pi/n_fibers_; // angular separation between fibers
-    // G4double dif_phi = 2*pi/n_fibers_; // angular separation between fibers
-    G4double rad = n_fibers_*radius_/pi; // radii of the big cylinder of fibers
-    G4double ang_pos;
+    G4double n_fibers = radius_cyl_*pi/radius_;
+    G4double dif_theta = .5*2*pi/n_fibers; // angular separation between fibers
+    G4double theta;
     G4double x;
     G4double y;
     G4double z = 0.;
 
-    // for (int i=0; i<=2*n_fibers_; i++){
-    for (int i=0; i<=n_fibers_/0.5; i++){
-      // fiber3
+    for (int i=0; i<=2*n_fibers; i++){
+
       fiber_ = new GenericWLSFiber("Y11", true, radius_, length_, true, true, tpb, ps, true);
       fiber_->SetCoreOpticalProperties(opticalprops::Y11());
       fiber_->SetCoatingOpticalProperties(opticalprops::TPB());
       fiber_->Construct();
       fiber_logic = fiber_->GetLogicalVolume();
 
-      ang_pos = dif_phi*i;
-      x = rad * cos(ang_pos);
-      y = rad * sin(ang_pos);
+      theta = dif_theta*i;
+      x = radius_cyl_ * cos(theta);
+      y = radius_cyl_ * sin(theta);
       new G4PVPlacement(0,G4ThreeVector(x, y, z),fiber_logic,
                               fiber_logic->GetName(),lab_logic,true,0,true);
     }
@@ -103,21 +100,10 @@ void Fiber_cylinder::Construct(){
 
 
 G4ThreeVector Fiber_cylinder::GenerateVertex(const G4String& region) const {
-    // return cyl_vertex_gen_->GenerateVertex(region);
-    // G4ThreeVector vertex(1.,1.,1.);
-    G4ThreeVector vertex(0., 0., 0.);
-
-    // WORLD
-    if (region == "WHOLE_VOL") {
-      return vertex;
-    }
-    // else if (region == "AD_HOC") {
-    //   return specific_vertex_;
-    // }
-    else {
-      G4Exception("[Fiber_cylinder]", "GenerateVertex()", FatalException,
-		  "Unknown vertex generation region!");
-    }
-    return vertex;
+    return cyl_vertex_gen_->GenerateVertex(region);
+    
+    // // G4ThreeVector vertex(1.,1.,1.);
+    // G4ThreeVector vertex(0., 0., 0.);
+    // return vertex;
 
 }
