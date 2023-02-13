92ec54a0b9b1dff5466a3b993edbd5ea5658bea1
mariandbt <mariandelbarrio@gmail.com>
2023-02-13 14:39:52 +0100

    testing geometries placement

--- source/geometries/Fib_box_struct.cc
+++ source/geometries/Fib_box_struct.cc
@@ -295,7 +295,7 @@ G4ThreeVector Fib_box_struct::GenerateVertex(const G4String& region) const {
 
     // // G4ThreeVector vertex(1.,1.,1.);
     // G4ThreeVector vertex(box_xy_/2, box_xy_/2, 0.);
-    G4ThreeVector vertex(0., 0., 0.1);
+    G4ThreeVector vertex(0., 0., 1.);
 
     // WORLD
     if (region == "CENTER") {
